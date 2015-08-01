e3154aabe01e40c827875decad0c8be64d3954af
pollosp <pollo.es.pollo@gmail.com>
2015-08-01 10:27:00 +0000

    Route53

--- rsstoweb/rsstoweb.rb
+++ rsstoweb/rsstoweb.rb
@@ -5,9 +5,9 @@ require 'open-uri'
 require 'haml'
 require 'yaml'
 
-
 get '/' do
   @status_postmark = RSS::Parser.parse("http://feeds.feedburner.com/postmarkstatus", false)
   @status_ec2 = RSS::Parser.parse("http://status.aws.amazon.com/rss/ec2-us-east-1.rss", false)
+  @status_route53 = RSS::Parser.parse("http://status.aws.amazon.com/rss/route53.rss", false)
   haml :status
 end
